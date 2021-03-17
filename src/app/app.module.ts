5c5dc2af11ccb86d41feb11d0c15dc809d90cd86
NhatDuong1812 <64996930+NhatDuong1812@users.noreply.github.com>
2021-03-17 16:50:04 +0700

    Duong: complete list song

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EditsongComponent } from './song/editsong/editsong.component';
 import { CreatePlaylistComponent } from './playlist/create-playlist/create-playlist.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+// import { NavbarComponent } from "./shared/navbar/NavbarComponent";
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { ListsongComponent } from './song/listsong/listsong.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EditsongComponent,
-    CreatePlaylistComponent
+    CreatePlaylistComponent,
+    FooterComponent,
+    HeaderComponent,
+    // NavbarComponent,
+    SidebarComponent,
+    ListsongComponent
+    
   ],
   imports: [
     BrowserModule,
---
+++ src/app/model/song.ts
@@ -0,0 +1,8 @@
+export interface song{
+    id?: number;
+    avatar?: string;
+    name?: string;
+    author?: string;
+    creationTime?: string;
+    numberOfView?:string;
+}
