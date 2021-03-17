e126a1aac06bbc9327de5c312b95245910ee7180
NhatDuong1812 <64996930+NhatDuong1812@users.noreply.github.com>
2021-03-17 10:46:15 +0700

    Duong: tạo from người dùng tạo playlist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EditsongComponent } from './song/editsong/editsong.component';
+import { CreatePlaylistComponent } from './playlist/create-playlist/create-playlist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditsongComponent
+    EditsongComponent,
+    CreatePlaylistComponent
   ],
   imports: [
     BrowserModule,
