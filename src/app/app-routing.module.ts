37399f48a8a2595cdbb0db52e719e7737f4537a9
NhatDuong1812 <64996930+NhatDuong1812@users.noreply.github.com>
2021-03-19 09:58:33 +0700

    Duong:Update Template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditsongComponent } from './song/editsong/editsong.component';
 // import {PlaylistComponentComponent} from './component/playlist-component/playlist-component.component';
 
 const routes: Routes = [
   {
     path: 'playlists',
-    // component: PlaylistComponentComponent
+    component: EditsongComponent
   }
 ];
 
